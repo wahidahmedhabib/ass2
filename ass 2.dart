41ef33e01fa6894522e89f5e9483bfa18f45fc0c
wahidahmedhabib <wahidahmed221098@gmail.com>
2021-09-22 13:12:17 +0100

    initial commit

---
+++ ass 2.dart
@@ -0,0 +1,96 @@
+
+
+void main() {
+//   for (int i = 0; i < 5; i++) {
+//     print('hello ${i + 1}');
+//   }
+  
+  // TYPES OF OPERATORS
+  // 1.ARITHMATIC 
+  // 2.EQUALITY AND RELATION 
+  // 3.LOGICAL 
+
+  
+       // TYPES OF OPERATORS; 
+  print("QUESTION NO #01");
+  print("///// TYPES OF OPERATORS /////");
+      //  1.ARITHMATIC OPERATORS
+  
+  print("/////  1.ARITHMATIC OPERATORS");
+  var a=19;
+   var b=9;
+//     String="a+b";
+//    String="a-b";
+//   String="a*b";
+//   String="a/b";
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+  print(a~/b);
+  print(a%b);
+ // print(a??b);
+  
+  print("Incremet");
+  int x=1;
+  int y= ++x;
+  print(y);
+  print("Decrement");
+  
+  int u=5;
+  int v=--u;
+  print(u);
+  
+  //  2.EQUALITY & RELATION OPERATOR
+  print("//////  2.EQUALITY & RELATION OPERATOR");
+  print(a<b);
+  print(a>b);
+  print(a>=b);
+  print(a<=b);
+  print(a==b);
+  print(a!=b);
+  
+     //  3.LOGICAL OPERATOR
+  print("////   3.LOGICAL OPERATOR");
+  print(12>10 && 15>10);
+   print(12>20 && 23>20);
+  print(a>20 || b>25);
+    print(!(7>13));
+  print(a%b);
+  // QUESTION 2
+  
+  print("QUESTION NO # 02");
+  var c=2, d=1;
+  //  (--c,- --d,+ ++c,+d--  )
+  //    1 - 0   + 2 +2
+ // print((--c - --d + ++c +d--  ));
+  print(--c); // 1
+  print(--d); //  0
+  print(++d); //  2
+  print(d--);   //  1
+  print ((--c) - (--d));  //   1
+  print ((--c) - (--d)+(++d)); //  2
+  print ((--c) - (--d)+(++d)+(d--)); //3
+  print("QUESTION NO #3");
+  var tikt=600;
+  print(5*tikt); 
+  print("QUESTION NO #4");
+  print("---");
+    print("QUESTION NO #5");
+  print(" AND OPERATOR USE WHEN ALL CONDITION IS TRUE");
+   print(" ?? OPERATOR USE WHEN  CONDITION IS NULL OR NOT");
+   print("QUESTION NO #6");
+  print("DATA TYPE =1.NUMBER  2.STRING 3.BOOLEANS  4.LISTS 5.MAPS");
+  print("Number= int.double .num");
+  int age=23;
+  print(age);
+  double hig=23/3;
+  print(hig);
+  num wgt=65.98;
+  print(wgt);
+//   var tbl=2;
+//   for(var i=1;i<=10;++i);{
+//     print( i*tbl);
+//   }
+  
+}
